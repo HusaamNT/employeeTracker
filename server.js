fbb762880f852f6e2da7cff6fda5329821d16d09
Husaam NT <husnaeem@gmail.com>
2022-12-18 19:45:34 +0000

    removed sequelize

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const sequelize = require("sequelize");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,8 +6,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
 
 
-sequelize.sync({force:false}).then(()=>{
-    app.listen(PORT, ()=> console.log("Now listening"))
-})
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
