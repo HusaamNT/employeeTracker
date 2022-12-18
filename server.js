354ffc5f3d86e0ed6507269d0c649e5e1c7f25aa
Husaam NT <husnaeem@gmail.com>
2022-12-18 20:01:48 +0000

    added inquirer questions, async for inquirer, view departments and roles dbs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-
+const inquirer = require("inquirer");
 
 const db = mysql.createConnection(
     {
@@ -11,5 +11,33 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
   );
 
+const trackerQuestions = {
+    type: "list",
+    name: "employeeTracker",
+    message: "What would you like to view today?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+    ],
+  };
 
+const askQuestions = async function(){
+    await inquirer.prompt(trackerQuestions)
+};
 
+function viewDepartments() {
+    db.query("SELECT * FROM DEPARTMENTS", function(err, results){
+        console.log(results)
+    }
+)};
+
+function viewRoles() {
+    db.query("SELECT * ROLES", function(err, results){
+        console.log(results)
+    }
+)}; 
