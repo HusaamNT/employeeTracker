1205cf85d5d20161b566d23789db532f3aa8fd3d
Husaam NT <husnaeem@gmail.com>
2022-12-18 19:40:03 +0000

    added all tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,27 @@ DROP DATABASE IF EXISTS employee_db
 CREATE DATABASE employee_db
 
 use employee_db
-    
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title ARCHAR(30) NOT NULL,
+    salary decimal,
+    department_id INT,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    department_id INT
+)
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+
 sequelize.sync({force:false}).then(()=>{
     app.listen(PORT, ()=> console.log("Now listening"))
 })
